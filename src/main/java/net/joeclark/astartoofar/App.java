1648976996e97af7b6eaa3cdaf6d3567e8c8e0c3
joeclark-phd <joe@joeclark.net>
2020-02-29 09:37:30 -0500

    add fxml to POM

--- src/main/java/net/joeclark/astartoofar/App.java
+++ src/main/java/net/joeclark/astartoofar/App.java
@@ -1,23 +1,11 @@
 package net.joeclark.astartoofar;
 
-import javafx.animation.Interpolator;
-import javafx.animation.PathTransition;
-import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.shape.Ellipse;
-import javafx.scene.shape.Path;
-import javafx.scene.shape.Sphere;
 import javafx.stage.Stage;
-import javafx.util.Duration;
+
 
 import java.io.IOException;
 
